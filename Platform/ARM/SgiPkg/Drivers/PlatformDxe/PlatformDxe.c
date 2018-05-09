4b0b6e6349dcd78a490e53b152d89251daa955fe
Daniil Egranov <daniil.egranov@arm.com>
2018-05-09 14:20:10 +0530

    Platform/ARM/Sgi: add the initial set of acpi tables
    Add the initial version of Acpi tables for the SGI-575 platform which
    is required to boot the linux kernel up to a busybox prompt.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Daniil Egranov <daniil.egranov@arm.com>
    Signed-off-by: Thomas Abraham <thomas.abraham@arm.com>
    Message-Id: <1525855811-10208-6-git-send-email-thomas.abraham@arm.com>

--- Platform/ARM/SgiPkg/Drivers/PlatformDxe/PlatformDxe.c
+++ Platform/ARM/SgiPkg/Drivers/PlatformDxe/PlatformDxe.c
@@ -12,6 +12,7 @@
 *
 **/
 
+#include <Library/AcpiLib.h>
 #include <Library/DebugLib.h>
 
 EFI_STATUS
@@ -27,6 +28,12 @@ ArmSgiPkgEntryPoint (
   )
 {
   EFI_STATUS              Status;
+
+  Status = LocateAndInstallAcpiFromFv (&gSgi575AcpiTablesiFileGuid);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "PlatformDxe: Failed to install ACPI tables\n"));
+    return Status;
+  }
 
   // Install Virtio Block IO.
   if (FeaturePcdGet (PcdVirtioSupported) == TRUE) {
