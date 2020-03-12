6c9ad0555b4853c068575e52c6946ed1160bb4af
Wang, Jian J <jian.j.wang@intel.com>
2020-03-12 13:52:19 +0800

    SecurityPkg: add RpmcLib and VariableKeyLib public headers
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=2594
    
    RpmcLib.h and VariableKeyLib.h are header files required to access RPMC
    device and Key generator from platform. They will be used to ensure the
    integrity and confidentiality of NV variables.
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Chao Zhang <chao.b.zhang@intel.com>
    Cc: Nishant C Mistry <nishant.c.mistry@intel.com>
    Signed-off-by: Jian J Wang <jian.j.wang@intel.com>
    Message-Id: <20200312055221.1781-2-jian.j.wang@intel.com>

---
+++ SecurityPkg/Include/Library/RpmcLib.h
@@ -0,0 +1,50 @@
+/** @file
+  Public definitions for the Replay Protected Monotonic Counter (RPMC) Library.
+
+Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _RPMC_LIB_H_
+#define _RPMC_LIB_H_
+
+#include <Uefi/UefiBaseType.h>
+
+/**
+  Requests the current monotonic counter from the designated RPMC counter.
+
+  @param[in]    CounterIndex            The RPMC index.
+  @param[out]   CounterValue            A pointer to a buffer to store the RPMC value.
+
+  @retval       EFI_SUCCESS             The operation completed successfully.
+  @retval       EFI_INVALID_PARAMETER   The CounterValue pointer is is NULL or CounterIndex is invalid.
+  @retval       EFI_NOT_READY           The given RPMC at CounterIndex is not yet initialized.
+  @retval       EFI_DEVICE_ERROR        A device error occurred while attempting to update the counter.
+  @retval       EFI_UNSUPPORTED         Requesting the monotonic counter is not supported in the current boot configuration.
+**/
+EFI_STATUS
+EFIAPI
+RequestMonotonicCounter (
+  IN  UINT8   CounterIndex,
+  OUT UINT32  *CounterValue
+  );
+
+/**
+  Increments the designated monotonic counter in the SPI flash device by 1.
+
+  @param[in]    CounterIndex            The RPMC index.
+
+  @retval       EFI_SUCCESS             The operation completed successfully.
+  @retval       EFI_INVALID_PARAMETER   The given CounterIndex value is invalid.
+  @retval       EFI_NOT_READY           The given RPMC at CounterIndex is not yet initialized.
+  @retval       EFI_DEVICE_ERROR        A device error occurred while attempting to update the counter.
+  @retval       EFI_UNSUPPORTED         Incrementing the monotonic counter is not supported in the current boot configuration.
+**/
+EFI_STATUS
+EFIAPI
+IncrementMonotonicCounter (
+  IN  UINT8   CounterIndex
+  );
+
+#endif
---
+++ SecurityPkg/Include/Library/VariableKeyLib.h
@@ -0,0 +1,59 @@
+/** @file
+  Public definitions for Variable Key Library.
+
+Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _VARIABLE_KEY_LIB_H_
+#define _VARIABLE_KEY_LIB_H_
+
+#include <Uefi/UefiBaseType.h>
+
+/**
+  Retrieves the variable root key.
+
+  @param[out]     VariableRootKey         A pointer to pointer for the variable root key buffer.
+  @param[in,out]  VariableRootKeySize     The size in bytes of the variable root key.
+
+  @retval       EFI_SUCCESS             The variable root key was returned.
+  @retval       EFI_DEVICE_ERROR        An error occurred while attempting to get the variable root key.
+  @retval       EFI_ACCESS_DENIED       The function was invoked after locking the key interface.
+  @retval       EFI_UNSUPPORTED         The variable root key is not supported in the current boot configuration.
+**/
+EFI_STATUS
+EFIAPI
+GetVariableRootKey (
+      OUT VOID    **VariableRootKey,
+  IN  OUT UINTN   *VariableRootKeySize
+  );
+
+/**
+  Regenerates the variable root key.
+
+  @retval       EFI_SUCCESS             The variable root key was regenerated successfully.
+  @retval       EFI_DEVICE_ERROR        An error occurred while attempting to regenerate the root key.
+  @retval       EFI_ACCESS_DENIED       The function was invoked after locking the key interface.
+  @retval       EFI_UNSUPPORTED         Key regeneration is not supported in the current boot configuration.
+**/
+EFI_STATUS
+EFIAPI
+RegenerateKey (
+  VOID
+  );
+
+/**
+  Locks the regenerate key interface.
+
+  @retval       EFI_SUCCESS             The key interface was locked successfully.
+  @retval       EFI_UNSUPPORTED         Locking the key interface is not supported in the current boot configuration.
+  @retval       Others                  An error occurred while attempting to lock the key interface.
+**/
+EFI_STATUS
+EFIAPI
+LockKeyInterface (
+  VOID
+  );
+
+#endif
