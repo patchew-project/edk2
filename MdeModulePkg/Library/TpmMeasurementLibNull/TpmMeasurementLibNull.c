a1bf6c5a9e1fe9d8d0683c34e7ded07c55d08c21
Jiewen Yao <jiewen.yao@intel.com>
2020-08-06 08:33:35 +0800

    MdeModulePkg/NullTpmMeasurementLib: Add new API.
    REF: https://bugzilla.tianocore.org/show_bug.cgi?id=2376
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Hao A Wu <hao.a.wu@intel.com>
    Cc: Qi Zhang <qi1.zhang@intel.com>
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>
    Message-Id: <20200806003342.17866-3-qi1.zhang@intel.com>

--- MdeModulePkg/Library/TpmMeasurementLibNull/TpmMeasurementLibNull.c
+++ MdeModulePkg/Library/TpmMeasurementLibNull/TpmMeasurementLibNull.c
@@ -1,10 +1,12 @@
 /** @file
   This library is used by other modules to measure data to TPM.
 
-Copyright (c) 2015, Intel Corporation. All rights reserved. <BR>
+Copyright (c) 2015 - 2020, Intel Corporation. All rights reserved. <BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
+
+#include <Uefi.h>
 
 /**
   Tpm measure and log data, and extend the measurement result into a specific PCR.
@@ -30,6 +32,63 @@ TpmMeasureAndLogData (
   IN UINT32             LogLen,
   IN VOID               *HashData,
   IN UINT64             HashDataLen
+  )
+{
+  //
+  // Do nothing, just return EFI_SUCCESS.
+  //
+  return EFI_SUCCESS;
+}
+
+/**
+  Mesure a FirmwareBlob.
+
+  @param[in]  Descrption              Description for this FirmwareBlob.
+  @param[in]  FirmwareBlobBase        Base address of this FirmwareBlob.
+  @param[in]  FirmwareBlobLength      Size in bytes of this FirmwareBlob.
+
+  @retval EFI_SUCCESS           Operation completed successfully.
+  @retval EFI_UNSUPPORTED       TPM device not available.
+  @retval EFI_OUT_OF_RESOURCES  Out of memory.
+  @retval EFI_DEVICE_ERROR      The operation was unsuccessful.
+*/
+EFI_STATUS
+EFIAPI
+MeasureFirmwareBlob (
+  IN UINT32                         PcrIndex,
+  IN CHAR8                          *Description OPTIONAL,
+  IN EFI_PHYSICAL_ADDRESS           FirmwareBlobBase,
+  IN UINT64                         FirmwareBlobLength
+  )
+{
+  //
+  // Do nothing, just return EFI_SUCCESS.
+  //
+  return EFI_SUCCESS;
+}
+
+/**
+  Mesure a HandoffTable.
+
+  @param[in]  PcrIndex                PcrIndex of the measurment.
+  @param[in]  Descrption              Description for this HandoffTable.
+  @param[in]  TableGuid               GUID of this HandoffTable.
+  @param[in]  TableAddress            Base address of this HandoffTable.
+  @param[in]  TableLength             Size in bytes of this HandoffTable.
+
+  @retval EFI_SUCCESS           Operation completed successfully.
+  @retval EFI_UNSUPPORTED       TPM device not available.
+  @retval EFI_OUT_OF_RESOURCES  Out of memory.
+  @retval EFI_DEVICE_ERROR      The operation was unsuccessful.
+*/
+EFI_STATUS
+EFIAPI
+MeasureHandoffTable (
+  IN UINT32                         PcrIndex,
+  IN CHAR8                          *Description OPTIONAL,
+  IN EFI_GUID                       *TableGuid,
+  IN VOID                           *TableAddress,
+  IN UINTN                          TableLength
   )
 {
   //
