70ea311e63ddd29779ee7b09dd7a6042da331fa1
Ilias Apalodimas <ilias.apalodimas@linaro.org>
2021-02-12 19:34:58 +0200

    Drivers/OpTeeRpmb: Add an OP-TEE backed RPMB driver
    A following patch is adding support for building StMM in order to run it
    from OP-TEE.
    OP-TEE in combination with a NS-world supplicant can use the RPMB
    partition of an eMMC to store EFI variables. The supplicant
    functionality is currently available in U-Boot only but can be ported
    into EDK2. Assuming similar functionality is added in EDK2, this will
    allow any hardware with an RPMB partition to store EFI variables
    securely.
    
    So let's add a driver that enables access of the RPMB partition through
    OP-TEE. Since the upper layers expect a byte addressable interface,
    the driver allocates memory and patches the PCDs, while syncing the
    memory/hardware on read/write callbacks.
    
    Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Message-Id: <20210212173459.508205-2-ilias.apalodimas@linaro.org>

---
+++ Drivers/OpTeeRpmb/OpTeeRpmbFvb.h
@@ -0,0 +1,52 @@
+/** @file
+
+  Copyright (c) 2020, Linaro Ltd. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef OPTEE_RPMB_FVB_H_
+#define OPTEE_RPMB_FVB_H_
+
+/**
+ Those are not currently defined in any spec, it's an internal
+ contract between OP-TEE and EDK2.
+ For more details check core/arch/arm/include/kernel/stmm_sp.h in OP-TEE
+**/
+#define SP_SVC_RPMB_READ                0xC4000066
+#define SP_SVC_RPMB_WRITE               0xC4000067
+
+#define FLASH_SIGNATURE            SIGNATURE_32('r', 'p', 'm', 'b')
+#define INSTANCE_FROM_FVB_THIS(a)  CR(a, MEM_INSTANCE, FvbProtocol, \
+                                      FLASH_SIGNATURE)
+
+typedef struct _MEM_INSTANCE         MEM_INSTANCE;
+typedef EFI_STATUS (*MEM_INITIALIZE) (MEM_INSTANCE* Instance);
+
+/**
+  This struct is used by the RPMB driver. Since the upper EDK2 layers
+  expect byte addressable memory, we allocate a memory area of certain
+  size and sync it to the hardware on reads/writes.
+
+  @param[in] Signature        Internal signature used to discover the instance
+  @param[in] Initialize       Function used to initialize the instance
+  @param[in] Initialized      Set to true if initialized
+  @param[in] FvbProtocol      FVB protocol of the instance
+  @param[in] Handle           Handle to install
+  @param[in] MemBaseAddress   Physical address of the beggining of the allocated memory
+  @param[in] BlockSize        Blocksize
+  @param[in] NBlocks          Number of allocated blocks
+**/
+struct _MEM_INSTANCE
+{
+    UINT32                              Signature;
+    MEM_INITIALIZE                      Initialize;
+    BOOLEAN                             Initialized;
+    EFI_FIRMWARE_VOLUME_BLOCK_PROTOCOL  FvbProtocol;
+    EFI_HANDLE                          Handle;
+    EFI_PHYSICAL_ADDRESS                MemBaseAddress;
+    UINT16                              BlockSize;
+    UINT16                              NBlocks;
+};
+
+#endif
